22170e1259c33e190b16d12ad9601db144b0bee6
Marco <marco.carone@libero.it>
2019-12-18 16:49:06 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
 // chiedo all’utente il numero di chilometri che vuole percorrere
 var chilometriTragitto = parseInt(prompt("Quanti chilometri devi percorrere?"));
-// chiedo la sua età
-var etaPasseggero = parseInt(prompt("Quanti anni hai?"));
-// stabilisco il costo del biglietto per 1km
-var costoUnChilometro = 0.21;
-// stabilisco il costo del biglietto in riferimento ai km inseriti dall'utente
-var costoBiglietto = chilometriTragitto * costoUnChilometro;
-// stabilisco gli sconti
-var scontoVenti = (costoBiglietto * 20) / 100;
-var scontoQuaranta = (costoBiglietto * 40) / 100;
-// creo una condizione
-if (isNaN(chilometriTragitto) || isNaN(etaPasseggero)) {
-  alert("Puoi inserire soltanto dei numeri. Riprova")
+if (isNaN(chilometriTragitto)) {
+  alert("Puoi inserire soltanto dei numeri. Riprova");
 } else {
-  if (etaPasseggero < 18) {
-    costoBiglietto = costoBiglietto - scontoVenti;
-  } else if (etaPasseggero > 65) {
-    costoBiglietto = costoBiglietto - scontoQuaranta;
+  // chiedo la sua età
+  var etaPasseggero = parseInt(prompt("Quanti anni hai?"));
+  if (isNaN(etaPasseggero)) {
+    alert("Puoi inserire soltanto dei numeri. Riprova");
   } else {
-    costoBiglietto;
+    var costoUnChilometro = 0.21;
+    // stabilisco il costo del biglietto in riferimento ai km inseriti dall'utente
+    var costoBiglietto = chilometriTragitto * costoUnChilometro;
+    // stabilisco gli sconti
+    var scontoVenti = (costoBiglietto * 20) / 100;
+    var scontoQuaranta = (costoBiglietto * 40) / 100;
+    // creo una condizione
+    if (isNaN(chilometriTragitto) || isNaN(etaPasseggero)) {
+      alert("Puoi inserire soltanto dei numeri. Riprova");
+    } else {
+      if (etaPasseggero < 18) {
+        costoBiglietto = costoBiglietto - scontoVenti;
+      } else if (etaPasseggero > 65) {
+        costoBiglietto = costoBiglietto - scontoQuaranta;
+      } else {
+        costoBiglietto;
+      }
+    }
+
+    // creo condizione sconto da visualizzare in riepilogo dati
+
+    // stampo il costo del biglietto
+    document.getElementById("costo-biglietto").innerHTML = costoBiglietto;
+
+    // stampo il riepilogo Dati
+    document.getElementById("chilometri-tragitto").innerHTML = chilometriTragitto;
+    document.getElementById("eta-passeggero").innerHTML = etaPasseggero;
   }
 }
-
-// creo condizione sconto da visualizzare in riepilogo dati
-
-// stampo il costo del biglietto
-document.getElementById("costo-biglietto").innerHTML = costoBiglietto;
-
-// stampo il riepilogo Dati
-document.getElementById("chilometri-tragitto").innerHTML = chilometriTragitto;
-document.getElementById("eta-passeggero").innerHTML = etaPasseggero;
